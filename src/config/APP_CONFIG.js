07a6368e3f5fad01fdb0d60705c2a151b74c8f23
Sultan Abdussalam <abdussultan14@gmail.com>
2025-10-23 13:28:27 +0100

    Initial Commit

---
+++ src/config/APP_CONFIG.js
@@ -0,0 +1,26 @@
+// export for all env credentials
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    PORT: process.env.PORT || 3000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+
+    JWT_SECRET: process.env.JWT_SECRET,
+    OTP_SECRET: process.env.OTP_SECRET,
+
+    EMAIL_SERVICE_SMTP_HOST: process.env.EMAIL_SERVICE_SMTP_HOST,
+    EMAIL_SERVICE_USER: process.env.EMAIL_SERVICE_USER,
+    EMAIL_SERVICE_APP_PASSWORD: process.env.EMAIL_SERVICE_APP_PASSWORD,
+    EMAIL_SERVICE_PORT: process.env.EMAIL_SERVICE_PORT,
+    PINO_LOG_LEVEL_CONSOLE: process.env.PINO_LOG_LEVEL_CONSOLE,
+    PINO_LOG_LEVEL_FILE: process.env.PINO_LOG_LEVEL_FILE,
+};
+
+
---
+++ src/config/sequelize.js
@@ -0,0 +1,18 @@
+// Sequelize setup
+import { Sequelize } from 'sequelize';
+import APP_CONFIG from './APP_CONFIG.js';
+
+const sequelize = new Sequelize(APP_CONFIG.DB_NAME, APP_CONFIG.DB_USER, APP_CONFIG.DB_PASS, {
+  host: APP_CONFIG.DB_HOST,
+  dialect: 'mysql',
+  port: APP_CONFIG.DB_PORT || 3306,
+});
+
+try{
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+}catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+export default sequelize;
